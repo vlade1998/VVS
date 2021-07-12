d50e527054caf3a5cfe4fa36f09fc14e5c6ad532
Vlademir <vlademir.santos@smarthis.com.br>
2021-07-11 21:11:04 -0300

    Removing java code

--- ATM/ATM.py
+++ ATM/ATM.py
@@ -24,31 +24,3 @@ class ATM:
         return ret
 
 
-
-    #     public static List<Integer[]> solutions(int[] values, int[] ammounts, int[] variation, int price, int position){
-    #     List<Integer[]> list = new ArrayList<>();
-    #     int value = compute(values, variation);
-    #     if (value < price){
-    #         for (int i = position; i < values.length; i++) {
-    #             if (ammounts[i] > variation[i]){
-    #                 int[] newvariation = variation.clone();
-    #                 newvariation[i]++;
-    #                 List<Integer[]> newList = solutions(values, ammounts, newvariation, price, i);
-    #                 if (newList != null){
-    #                     list.addAll(newList);
-    #                 }
-    #             }
-    #         }
-    #     } else if (value == price) {
-    #         list.add(myCopy(variation));
-    #     }
-    #     return list;
-    # }    
-
-    #  public static int compute(int[] values, int[] variation){
-    #     int ret = 0;
-    #     for (int i = 0; i < variation.length; i++) {
-    #         ret += values[i] * variation[i];
-    #     }
-    #     return ret;
-    # }    
